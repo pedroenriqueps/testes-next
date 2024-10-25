5d560f33104b70f31268a2aa74ccbcd9f8ef748d
peps111-crypto <minionbart50@gmail.com>
2024-10-25 18:10:46 -0300

    ajustando middware para pasta correta

---
+++ src/middleware.tsx
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export const TOKEN_KEY = "token"
+
+export async function middleware(request: NextRequest) {
+    const cookie = await cookies()
+    const token = cookie.get(TOKEN_KEY)
+
+    const protectedRoutes = ["/private-route/dashboard", "/private-route/settings"]
+
+    const isProtectedRoute = protectedRoutes.includes(request.nextUrl.pathname)
+
+    if (isProtectedRoute && !token) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ["/private-route/dashboard/", "/private-route/settings/"]
+}
